a16a1ebec974647a3b9c921522fde34615face3f
Vijayant Pandey <119390705+vijayant-pandey@users.noreply.github.com>
2025-09-17 12:27:28 +0530

    Update authRoutes.js
    Deleted the commented COdes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -22,19 +22,3 @@ router.get("/me", protect, (req, res) => {
 
 export default router;
 
-
-// How Middleware Works
-
-// When you login, your backend sends a token.
-
-// That token proves your identity (like a digital key).
-
-// When you call /api/auth/me, the middleware:
-
-// Checks Authorization: Bearer <token> in headers.
-
-// Verifies it with jwt.verify.
-
-// Attaches the user (req.user) so routes know who you are.
-
-// If no token or invalid token → request is rejected.
