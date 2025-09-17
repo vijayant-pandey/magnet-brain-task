52ab75265f32412d767817132ad548bf2f8029c4
Vijayant Pandey <119390705+vijayant-pandey@users.noreply.github.com>
2025-09-17 12:25:52 +0530

    Update authMiddleware.js
    Deleted the COmmented Codes

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,24 +1,3 @@
-// import jwt from 'jsonwebtoken';
-// import User from '../models/User.js';
-
-// export const protect = async (req, res, next) => {
-//   let token;
-//   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-//     try {
-//       token = req.headers.authorization.split(' ')[1];
-//       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//       // attach user to request (without password)
-//       req.user = await User.findById(decoded.id).select('-password');
-//       next();
-//     } catch (err) {
-//       console.error(err);
-//       return res.status(401).json({ message: 'Not authorized, token failed' });
-//     }
-//   } else {
-//     return res.status(401).json({ message: 'Not authorized, no token' });
-//   }
-// };
-
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 
